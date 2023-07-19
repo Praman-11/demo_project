8dc98c457d22337debadcefc030dda85eb3075af
praman11 <pramanchouhan9174@gmail.com>
2023-07-19 16:42:15 +0530

    just_dial

--- config/routes.rb
+++ config/routes.rb
@@ -8,20 +8,10 @@ Rails.application.routes.draw do
   # --------------service_routes-----------------
   resources :service
   delete "/delete/service/:id", to: "service#destroy"
-  # get "/services/search", to: "service#show_service"
   # --------------user_routes--------------------
   resources :users
   post "/auth/user/login", to: "user_authentication#login"
-  # --------------customer_routes----------------
-  # resources :customer
-  # post "/auth/customer/login", to: "customer_authentication#login"
-  # # --------------admin_routes-------------------
-  # resources :admin
-  # post "/auth/admin/login", to: "admin_authentication#login"
-  # -------------rating_routes-------------------
+# -------------rating_routes-------------------
   resources :rating, only: [:create]
-  get "/rating/search_city_wise", to: "rating#search_city_wise"
-  get "/rating/search_nearby_location", to: "rating#search_nearby_location"
-  get "sort_listing_service_based_on_rating", to: "rating#sort_listing_service_on_rating"
-  get "filter_services_on_rating", to: "rating#filter_services_on_rating"
+  
 end
